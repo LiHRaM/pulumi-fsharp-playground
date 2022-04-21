99357f0fbf58ea10935b7858ce1b36ce491070cb
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-04-21 21:05:19 +0200

    Demonstrate Kubernetes provider case

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿module Program
+
+open Pulumi
+
+let infra () =
+  let provider1 = Kubernetes.Provider(
+    "provider1",
+    args = Kubernetes.ProviderArgs()
+  )
+
+  let provider2 = FSharp.Kubernetes.provider {
+    name "provider2"
+  }
+
+  dict []
+
+FSharp.Deployment.run infra |> ignore
